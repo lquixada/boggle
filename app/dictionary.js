f6944eaf032fdee8d8e7c01b99e2f6417d226a5c
Leonardo Quixada <lquixada@gmail.com>
2016-01-03 16:04:15 -0200

    dictionary fix

--- app/dictionary.js
+++ app/dictionary.js
@@ -9,7 +9,7 @@ Dictionary.prototype = {
   check: function (word, cb) {
     var url = 'https://en.wiktionary.org/w/api.php?action=query&format=json&callback=?&titles=';
 
-    return $.getJSON(url+word, function (data) {
+    return $.getJSON(url+word.toLowerCase(), function (data) {
       cb(!data.query.pages[-1]);
     });
   }
