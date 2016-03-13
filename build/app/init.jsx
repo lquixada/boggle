d22982f0a846088ee4ecaf93cc00212ba3abb21f
Leonardo Quixada <lquixada@gmail.com>
2016-03-13 17:59:16 -0300

    require text plugin removed.

--- build/app/init.jsx
+++ build/app/init.jsx
@@ -1,7 +1,6 @@
 requirejs.config({
   baseUrl: location.href,
   paths: {
-    'text': 'vendor/text/text',
     'jquery': 'vendor/jquery/dist/jquery.min',
     'jquery.knob': 'vendor/jquery-knob/dist/jquery.knob.min',
     'underscore': 'vendor/underscore/underscore-min',
