c4add8965d33a80e1f0deb2c470fae3eb973463a
Leonardo Quixada <lquixada@gmail.com>
2016-06-04 23:25:35 -0300

    react hot reloading working.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from '../reducers';
-import App from './app';
-import css from '../styles/index.less';
+import reducers from './reducers';
+import App from './components/app';
+import css from './index.less';
 
 const store = createStore(reducers);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './components',
-  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080/', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './index.js'
+  ],
+  devtool: 'eval',
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'] },
       { test: /\.less$/, loader: 'style!css!less' }
     ]
   },
   output: {
     path: __dirname,
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
