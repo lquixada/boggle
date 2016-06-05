0199487645213d3f6b5e620b0cac1a2712ce74a3
Leonardo Quixada <lquixada@gmail.com>
2016-06-04 23:31:15 -0300

    eslint now is the only linter.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,13 @@
 module.exports = function(grunt) {
-  var readJSON = function (filePath) {
-    // Read the JSON file as a regular file
-    var str = grunt.file.read(filePath);
-
-    // Remove comments
-    str = str.replace(/\/\/.*/mg, '');
-
-    // Returns a valid JSON
-    return JSON.parse(str);
-  };
-
+  
   // Project configuration.
   grunt.initConfig({
     githooks: {
       dist: {
         'pre-push': 'review'
       }
-    },
-
-    jshint: {
-      dist: {
-        src: ['app/**/*.js'],
-        options: readJSON('config/.jshintrc')
-      }
-    },
-
-    jscs: {
-      dist: {
-        src: 'app/**/*.js',
-        options: readJSON('config/.jscsrc')
-      }
     }
   });
-
-  /**
-   * Defining aliases
-   */
-  grunt.registerTask('lint', ['jshint', 'jscs']);
 
   /**
    * Loading tasks
