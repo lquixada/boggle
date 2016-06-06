f48278a9a8f9988d0cae776f204da34b45078290
Leonardo Quixada <lquixada@gmail.com>
2016-06-05 23:27:05 -0300

    tests passing and simplified.

--- tests/button.spec.js
+++ tests/button.spec.js
@@ -4,16 +4,21 @@ var sinon = require('sinon');
 var expect = require('expect');
 var Button = require('../components/button.js');
 
+// Removes Connect wrapper
+Button = Button.default.WrappedComponent
 
 describe('<Button />', function () {
 
-  it('simulates click events', function () {
-    var onButtonClick = sinon.spy();
-    var wrapper = shallow(<Button started="true" onClick={onButtonClick} />);
+  it('shows "start" when the game is stopped', function () {
+    var component = shallow(<Button started={false} />);
 
-    wrapper.find('button').simulate('click');
+    expect(component.find('button').text()).toEqual('start');
+  });
 
-    expect(onButtonClick.calledOnce).toEqual(true);
+  it('shows "stop" when the game has started', function () {
+    var component = shallow(<Button started={true} />);
+
+    expect(component.find('button').text()).toEqual('stop');
   });
 
 });
