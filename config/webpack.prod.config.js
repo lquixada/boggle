7849d9a50b0673dabd549241c221e08892cf63ab
Leonardo Quixada <lquixada@gmail.com>
2016-06-08 15:25:34 -0300

    bundle.js generated with NODE_ENV=production.

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,16 +1,26 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
+
   devtool: 'source-map',
+
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
       { test: /\.less$/, loaders: ['style', 'css', 'less'] }
     ]
   },
+
   output: {
     path: path.join(__dirname, '..'),
     filename: 'bundle.js'
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+    })
+  ]
 };
