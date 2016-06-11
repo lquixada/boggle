9057c538c33bcd6d778f588c061664cbc11717bc
Leonardo Quixada <lquixada@gmail.com>
2016-06-11 16:07:55 -0300

    added utils.spec.js

--- tests/board.spec.js
+++ tests/board.spec.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import mountConnected from './helper';
-import { List } from 'immutable';
 import { bindActionCreators } from 'redux'
 import configureStore from '../store';
 import * as actionCreators from '../actions';
-import BoardChecker from '../utils/board-checker';
 import Board from '../components/board';
 
 describe('<Board />', () => {
@@ -34,78 +32,6 @@ describe('<Board />', () => {
     actions.stopGame();
     const text = component.find('table').text();
     expect(text.trim()).to.be.empty;
-  });
-
-});
-
-describe('BoardChecker', () => {
-  let actions;
-  let store;
-  let state;
-
-  beforeEach(() => {
-    store = configureStore();
-    actions = bindActionCreators(actionCreators, store.dispatch);
-  });
-
-  it('finds regular word', () => {
-    const matrix = List([
-      List(['O', 'D', 'E', 'E']),
-      List(['E', 'E', 'O', 'S']),
-      List(['N', 'O', 'R', 'M']),
-      List(['A', 'I', 'X', 'V'])
-    ]);
-    const board = new BoardChecker(matrix);
-
-    expect(board.hasWord('norm')).to.be.equal(true);
-  });
-
-  it('finds backwards word', () => {
-    const matrix = List([
-      List(['O', 'D', 'E', 'E']),
-      List(['E', 'E', 'O', 'S']),
-      List(['M', 'R', 'O', 'N']),
-      List(['A', 'I', 'X', 'V'])
-    ]);
-    const board = new BoardChecker(matrix);
-
-    expect(board.hasWord('norm')).to.be.equal(true);
-  });
-
-  it('finds word on diagonal', () => {
-    const matrix = List([
-      List(['N', 'D', 'E', 'E']),
-      List(['E', 'O', 'O', 'S']),
-      List(['M', 'R', 'R', 'N']),
-      List(['A', 'I', 'X', 'M'])
-    ]);
-    const board = new BoardChecker(matrix);
-
-    expect(board.hasWord('norm')).to.be.equal(true);
-  });
-
-  it('finds word on backwards diagonal', () => {
-    const matrix = List([
-      List(['O', 'D', 'E', 'N']),
-      List(['E', 'O', 'O', 'S']),
-      List(['T', 'R', 'R', 'N']),
-      List(['M', 'I', 'X', 'M'])
-    ]);
-    const board = new BoardChecker(matrix);
-
-    expect(board.hasWord('norm')).to.be.equal(true);
-  });
-
-  it('finds circular word', () => {
-    const matrix = List([
-      List(['O', 'D', 'E', 'N']),
-      List(['E', 'O', 'N', 'S']),
-      List(['T', 'R', 'M', 'N']),
-      List(['M', 'I', 'X', 'M'])
-    ]);
-    const board = new BoardChecker(matrix);
-
-    expect(board.hasWord('norm')).to.be.equal(true);
   });
 
 });
---
+++ tests/utils.spec.js
@@ -0,0 +1,77 @@
+import { List } from 'immutable';
+import { bindActionCreators } from 'redux'
+import configureStore from '../store';
+import * as actionCreators from '../actions';
+import BoardChecker from '../utils/board-checker';
+
+describe('BoardChecker', () => {
+  let actions;
+  let store;
+  let state;
+
+  beforeEach(() => {
+    store = configureStore();
+    actions = bindActionCreators(actionCreators, store.dispatch);
+  });
+
+  it('finds regular word', () => {
+    const matrix = List([
+      List(['O', 'D', 'E', 'E']),
+      List(['E', 'E', 'O', 'S']),
+      List(['N', 'O', 'R', 'M']),
+      List(['A', 'I', 'X', 'V'])
+    ]);
+    const board = new BoardChecker(matrix);
+
+    expect(board.hasWord('norm')).to.be.equal(true);
+  });
+
+  it('finds backwards word', () => {
+    const matrix = List([
+      List(['O', 'D', 'E', 'E']),
+      List(['E', 'E', 'O', 'S']),
+      List(['M', 'R', 'O', 'N']),
+      List(['A', 'I', 'X', 'V'])
+    ]);
+    const board = new BoardChecker(matrix);
+
+    expect(board.hasWord('norm')).to.be.equal(true);
+  });
+
+  it('finds word on diagonal', () => {
+    const matrix = List([
+      List(['N', 'D', 'E', 'E']),
+      List(['E', 'O', 'O', 'S']),
+      List(['M', 'R', 'R', 'N']),
+      List(['A', 'I', 'X', 'M'])
+    ]);
+    const board = new BoardChecker(matrix);
+
+    expect(board.hasWord('norm')).to.be.equal(true);
+  });
+
+  it('finds word on backwards diagonal', () => {
+    const matrix = List([
+      List(['O', 'D', 'E', 'N']),
+      List(['E', 'O', 'O', 'S']),
+      List(['T', 'R', 'R', 'N']),
+      List(['M', 'I', 'X', 'M'])
+    ]);
+    const board = new BoardChecker(matrix);
+
+    expect(board.hasWord('norm')).to.be.equal(true);
+  });
+
+  it('finds circular word', () => {
+    const matrix = List([
+      List(['O', 'D', 'E', 'N']),
+      List(['E', 'O', 'N', 'S']),
+      List(['T', 'R', 'M', 'N']),
+      List(['M', 'I', 'X', 'M'])
+    ]);
+    const board = new BoardChecker(matrix);
+
+    expect(board.hasWord('norm')).to.be.equal(true);
+  });
+
+});
