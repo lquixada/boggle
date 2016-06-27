e5dfb07f8935f6dd2fbd816d5cf548b033087b74
Leonardo Quixada <lquixada@gmail.com>
2016-06-27 17:12:09 -0300

    historyFallback enabled on dev mode.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -32,7 +32,7 @@ module.exports = {
 
     // Set this as true if you want to access dev server from arbitrary url.
     // This is handy if you are using a html5 router.
-    historyApiFallback: false,
+    historyApiFallback: true,
 
     // Set this if you want to enable gzip compression for assets
     // compress: true,
