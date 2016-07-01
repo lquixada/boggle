37cb125937861f1c7f4d0dc79dcb4e71cb58b67c
Leonardo Quixada <lquixada@gmail.com>
2016-06-30 21:55:04 -0300

    template engine used to prevent duplicate html.

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ const configureStore = require('./shared/store').default;
 const PORT = process.env.PORT || 9000;
 const app = express();
 
-
 if (process.env.NODE_ENV === 'development') {
   const webpack = require('webpack');
-  const config = require('./webpack.dev.config.js');
+  const config = require('./webpack.dev.config');
   const compiler = webpack(config);
 
   app.use(require('webpack-dev-middleware')(compiler, {
@@ -31,13 +30,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(require('webpack-hot-middleware')(compiler));
 }
 
+app.set('view engine', 'hbs');
+app.set('views', './shared');
 app.use(express.static(__dirname, {
   // Ignore the static index.html and generate a dynamic one
   index: false
 }));
 
 app.use((req, res) => {
-  const store = configureStore();
 
   match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
     if (err) {
@@ -49,36 +49,15 @@ app.use((req, res) => {
       return res.status(404).end('Not found');
     }
 
+    const store = configureStore();
     const routing = React.createElement(RouterContext, renderProps);
     const provider = React.createElement(Provider, {store: store}, routing);
     const html = renderToString(provider);
 
-    res.end(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title>Boggle</title>
-
-          <link href="/assets/sheets/bundle.css" rel="stylesheet" />
-
-          <script>
-            window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())};
-          </script>
-        </head>
-        <body>
-          <div id="game">${html}</div>
-          <script src="/assets/scripts/bundle.js"></script>
-
-          <a class="ribbon" href="https://github.com/lquixada/boggle">
-            <img style="position: absolute; top: 0; right: 0; border: 0;" alt="Fork me on GitHub"
-            src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67"
-            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" />
-          </a>
-        </body>
-      </html>
-    `);
+    res.render('index', {
+      data: JSON.stringify(store.getState()),
+      html: renderToString(provider)
+    });
   });
 });
 
