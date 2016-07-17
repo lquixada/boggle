70e399c95182fa59f0b6daa16cf1d2b365441e95
Leonardo Quixadá <leonardo.quixada@b2wdigital.com>
2016-07-17 19:47:54 -0300

    uglify warnings suppressed

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -24,6 +24,11 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new ExtractTextPlugin('../sheets/bundle.css')
   ]
 };
