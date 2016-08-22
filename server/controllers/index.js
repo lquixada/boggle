1e6554dc1510a82f9aed8f3729ee4a3bceba975b
Leonardo Quixadá <leonardo.quixada@b2wdigital.com>
2016-08-22 12:37:48 -0300

    separated controller from app.

---
+++ server/controllers/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+
+import routes from '../../shared/routes';
+import configureStore from '../../shared/store';
+
+export default (req, res) => {
+  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end('Internal server error');
+    }
+
+    if (!renderProps) {
+      return res.status(404).end('Not found');
+    }
+
+    const store = configureStore();
+    const routing = React.createElement(RouterContext, renderProps);
+    const provider = React.createElement(Provider, {store: store}, routing);
+
+    res.render('index', {
+      data: JSON.stringify(store.getState()),
+      html: renderToString(provider)
+    });
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,17 +1,8 @@
 require('babel-core/register');
 require.extensions['.scss'] = function () {};
 
-const express = require('express');
-const React = require('react');
-
-const { renderToString } = require('react-dom/server');
-const { match, RouterContext } = require('react-router');
-const { Provider } = require('react-redux');
-
-const routes = require('../shared/routes').default;
-const configureStore = require('../shared/store').default;
-
 const PORT = process.env.PORT || 9000;
+const express = require('express');
 const app = express();
 
 /* Setup */
@@ -33,27 +24,7 @@ app.use(require('./middlewares/logger').default);
 app.use(require('./middlewares/static').default);
 
 /* Controllers */
-app.use((req, res) => {
-  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).end('Internal server error');
-    }
-
-    if (!renderProps) {
-      return res.status(404).end('Not found');
-    }
-
-    const store = configureStore();
-    const routing = React.createElement(RouterContext, renderProps);
-    const provider = React.createElement(Provider, {store: store}, routing);
-
-    res.render('index', {
-      data: JSON.stringify(store.getState()),
-      html: renderToString(provider)
-    });
-  });
-});
+app.use(require('./controllers/index').default);
 
 const server = app.listen(PORT, () => {
   console.info(`Server running on: http://localhost:${PORT}/`);
