9c76ac87c3d76e969b39362ada1f73e5d44a5cef
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2016-09-22 10:56:50 -0300

    fixed double default exports on board.js

--- shared/components/board.js
+++ shared/components/board.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-export default class Board extends React.Component {
+class Board extends React.Component {
   render() {
     return (
       <div id="board" className="box">
