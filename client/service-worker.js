878cc7fd8ab045c0f130ee7cde6f9ea68ab8ddb3
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2016-09-25 19:57:38 -0300

    service-worker on ES6

--- client/service-worker.js
+++ client/service-worker.js
@@ -14,21 +14,21 @@ const cacheAssets = [
 /*
  * Installation
  */
-self.addEventListener('install', function (event) {
+self.addEventListener('install', event => {
   console.log('Event: install');
 
-  const cached = caches.open(cacheKey).then(function (cache) {
-    return cache.addAll(cacheAssets).then(function () {
+  const cached = caches.open(cacheKey).then(cache =>
+    cache.addAll(cacheAssets).then(() => {
       // Note: external assets need special request to be cached.
       const request = new Request('https://travis-ci.org/lquixada/boggle.svg?branch=master');
-      
-      return fetch(request, { mode: 'no-cors' }).then(function (response) {
-        // Note: cache.put needs to receive "request.url" instead of "request"
-        // in order to properly cache external assets.
-        return cache.put(request.url, response);
-      });
-    });
-  });
+
+      // Note: cache.put needs to receive "request.url" instead of "request"
+      // in order to properly cache external assets.      
+      return fetch(request, { mode: 'no-cors' }).then(response =>
+        cache.put(request.url, response)
+      );
+    })
+  );
 
   event.waitUntil(cached);
 });
@@ -37,15 +37,13 @@ self.addEventListener('install', function (event) {
 /*
  * Activation
  */
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', event => {
   console.log('Event: activate');
 
-  const cleaned = caches.keys().then(function (keys) {
-    const promises = keys.filter((key) => {
-      return key.startsWith('boggle-') && key !== cacheKey;
-    }).map((key) => {
-      return cache.delete(key);
-    });
+  const cleaned = caches.keys().then(keys => {
+    const promises = keys.filter(key => key.startsWith('boggle-'))
+      .filter(key => key !== cacheKey)
+      .map(key => cache.delete(key));
 
     return Promise.all(promises);
   });
@@ -57,19 +55,19 @@ self.addEventListener('activate', function (event) {
 /*
  * Fetchs
  */
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
   console.log('Event: fetch');
 
   // Note: caches.match needs to receive "request.url" instead of "request"
   // in order to properly cache external assets.
-  const fetched = caches.match(event.request.url).then(function (response) {
+  const fetched = caches.match(event.request.url).then(response => {
     if (response) {
       console.log('Cache', event.request.url);
       return response;
     }
 
     console.log('Fetched', event.request.url);
-    return fetch(event.request.clone(), { mode: 'no-cors' });
+    return fetch(event.request.clone(), {mode: 'no-cors'});
   });
 
   event.respondWith(fetched);
