fb4f0097c449faff46f38daa84768580d9e3d099
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2016-10-02 20:16:40 -0300

    added google analytics.

--- server/template.js
+++ server/template.js
@@ -17,6 +17,15 @@ export default ({state, content, assets, bundle}) => (`
     <body>
       <div id="game">${content}</div>
       <script src="${assets.main.js}"></script>
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-85035199-1', 'auto');
+        ga('send', 'pageview');
+      </script>
     </body>
   </html>
 `);
