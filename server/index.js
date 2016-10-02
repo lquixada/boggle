4f1c43081605cc102e1b5a8592d914835535f549
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2016-10-02 13:29:30 -0300

    added helmet to fix security issues.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import loggerMiddleware from './middlewares/logger';
 import staticMiddleware from './middlewares/static';
 import indexController from './controllers/index';
@@ -11,6 +12,7 @@ if (process.env.NODE_ENV === 'development') {
   const webpackMiddlewares = require('./middlewares/webpack').default;
   server.use(...webpackMiddlewares);
 }
+server.use(helmet());
 server.use(loggerMiddleware);
 server.use(staticMiddleware);
 
