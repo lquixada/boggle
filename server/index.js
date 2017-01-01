92c97db5e3798e74fea871860de7f22f77670fe6
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2017-01-01 18:54:48 -0200

    certbot controller.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import secureMiddleware from './middlewares/secure';
 import loggerMiddleware from './middlewares/logger';
 import staticMiddleware from './middlewares/static';
+import certbotController from './controllers/certbot';
 import indexController from './controllers/index';
 
 // For semantic purposes, app is equivalent to server.
@@ -27,6 +28,7 @@ server.use(loggerMiddleware);
 server.use(staticMiddleware);
 
 /* Controllers */
-server.use(indexController);
+server.use('/.well-known/acme-challenge/', certbotController);
+server.use('/', indexController);
 
 export default server;
