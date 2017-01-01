193bcb405d6234e276949e45052ec8b53f1d26e3
Leonardo Quixada <leonardo.quixada@b2wdigital.com>
2017-01-01 19:07:31 -0200

    new certbot key.

--- server/controllers/certbot.js
+++ server/controllers/certbot.js
@@ -1,3 +1,3 @@
 export default (req, res) => {
-  res.send('spz9gNt7ern1gKmDb4X3phQvats2dbLGemKSDl36Tq0.69bCnYYsQZKoAqYIy7V97R4RAr6UpNwn5Yiu0po1I8g');
+  res.send('9WvD89m0czpeaHEpGdVJ_4cfoOFopLhuB_PJ_V04kBk.69bCnYYsQZKoAqYIy7V97R4RAr6UpNwn5Yiu0po1I8g');
 };
