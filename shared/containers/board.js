7998815fa6f05fa2f2d794fa530e90be32efe545
Leonardo Quixadá <lquixada@gmail.com>
2017-07-30 10:55:16 -0300

    board and score containers simplified.

--- shared/containers/board.js
+++ shared/containers/board.js
@@ -1,15 +1,5 @@
-import React from 'react';
 import {connect} from 'react-redux';
 import Board from '../components/board';
-import * as actions from '../actions';
-
-class BoardContainer extends React.Component {
-  render() {
-    return (
-      <Board matrix={this.props.matrix} />
-    );
-  }
-}
 
 const mapStateToProps = ({matrix}) => ({matrix});
-export default connect(mapStateToProps)(BoardContainer);
+export default connect(mapStateToProps)(Board);
--- shared/containers/score.js
+++ shared/containers/score.js
@@ -1,15 +1,5 @@
-import React from 'react';
 import {connect} from 'react-redux';
 import Score from '../components/score';
 
-class ScoreContainer extends React.Component {
-  render() {
-    return (
-      <Score counter={this.props.counter} attempts={this.props.attempts} />
-    );
-  }
-}
-
 const mapStateToProps = ({attempts, counter}) => ({attempts, counter});
-
-export default connect(mapStateToProps)(ScoreContainer);
+export default connect(mapStateToProps)(Score);
