5671f0b4417968a8ca2c67446dcb255ca9a192af
Leonardo Quixadá <lquixada@gmail.com>
2018-04-07 15:46:23 -0300

    removed HotContainer component.

--- src/client/components.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {AppContainer} from 'react-hot-loader';
-
-export const HotContainer = ({children}) => {
-  if (module.hot) {
-    return <AppContainer>{children}</AppContainer>;
-  }
-  return children;
-};
--- src/client/index.js
+++ src/client/index.js
@@ -4,30 +4,30 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
 import {renderRoutes} from 'react-router-config';
+import {AppContainer} from 'react-hot-loader';
 
 import routes from '../shared/routes';
 import store from './store';
 import history from './history';
-import {HotContainer} from './components';
 
 const renderApp = () => {
   const routes = require('../shared/routes').default;
 
   ReactDOM.hydrate(
-    <HotContainer>
+    <AppContainer>
       <Provider store={store}>
         <ConnectedRouter history={history} children={renderRoutes(routes)} />
       </Provider>
-    </HotContainer>
+    </AppContainer>
     , document.getElementById('game')
   );
 };
 
+renderApp();
+
 if (module.hot) {
   module.hot.accept('../shared/routes', renderApp);
 }
-
-renderApp();
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js');
