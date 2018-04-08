c8a37803162edc1056ad810dba2324b49c7c7a46
Leonardo Quixadá <lquixada@gmail.com>
2018-04-08 10:50:52 -0300

    removed mountConnected from helpers.

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {mount} from 'enzyme';
-
 export const createServer = () => {
   const server = require('../server');
 
   return server.default.listen(8001);
 };
-
-export const mountConnected = (component, store) =>
-  mount(<Provider store={store}>
-    {component}
-  </Provider>);
