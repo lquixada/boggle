d2ba0251448a4e6e6db9af140698ac7085b4d8bf
Leonardo Quixadá <lquixada@gmail.com>
2018-04-08 13:37:36 -0300

    coverage threshold set.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  moduleNameMapper: {
+  'moduleNameMapper': {
     '\\.scss': 'identity-obj-proxy'
   },
-  testEnvironment: 'jsdom',
-  testMatch: [
+  'testEnvironment': 'jsdom',
+  'testMatch': [
     '<rootDir>/src/**/*.spec.js'
   ],
-  setupTestFrameworkScriptFile: '<rootDir>/src/__tests__/_setup/index.js',
-  coverageDirectory: '<rootDir>/.coverage'
+  'setupTestFrameworkScriptFile': '<rootDir>/src/__tests__/_setup/index.js',
+  'coverageDirectory': '<rootDir>/.coverage',
+  'coverageThreshold': {
+    'global': {
+      'branches': 60,
+      'functions': 70,
+      'lines': 70,
+      'statements': 60
+    }
+  }
 };
