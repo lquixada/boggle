999e4808d259d7ab805b40d4ab24a27fb04ede30
Leonardo Quixadá <lquixada@gmail.com>
2018-04-08 22:36:37 -0300

    actions specs grouped.

--- src/shared/actions/__tests__/index.spec.js
+++ src/shared/actions/__tests__/index.spec.js
@@ -6,79 +6,81 @@ import * as actionCreators from '../../actions';
 import * as helper from '../../utils/helper';
 import {startGame, stopGame} from '../';
 
-describe('startGame', () => {
-  it('returns an action to start the game', () => {
-    expect(startGame()).toEqual({
-      type: 'START_GAME',
+describe('actions', () => {
+  describe('startGame', () => {
+    it('returns an action to start the game', () => {
+      expect(startGame()).toEqual({
+        type: 'START_GAME',
+      });
     });
   });
-});
 
-describe('stopGame', () => {
-  it('returns an action to stop the game', () => {
-    expect(stopGame()).toEqual({
-      type: 'STOP_GAME',
+  describe('stopGame', () => {
+    it('returns an action to stop the game', () => {
+      expect(stopGame()).toEqual({
+        type: 'STOP_GAME',
+      });
     });
   });
-});
 
-describe('addCheckedAttempt', () => {
-  let actions;
-  let store;
+  describe('addCheckedAttempt', () => {
+    let actions;
+    let store;
 
-  beforeEach(() => {
-    const attempts = [
-      {word: 'HEY', score: 3},
-    ];
+    beforeEach(() => {
+      const attempts = [
+        {word: 'HEY', score: 3},
+      ];
 
-    const matrix = [
-      ['O', 'H', 'E', 'Y'],
-      ['E', 'E', 'O', 'S'],
-      ['N', 'O', 'R', 'M'],
-      ['A', 'I', 'X', 'V'],
-    ];
+      const matrix = [
+        ['O', 'H', 'E', 'Y'],
+        ['E', 'E', 'O', 'S'],
+        ['N', 'O', 'R', 'M'],
+        ['A', 'I', 'X', 'V'],
+      ];
 
-    store = configureStore({attempts, matrix});
-    actions = bindActionCreators(actionCreators, store.dispatch);
-    helper.isOnDictionary = jest.fn().mockReturnValue(true);
-  });
-
-  it('validates correct word', () => {
-    const promise = actions.addCheckedAttempt('norm').then(() => {
-      const {attempts} = store.getState();
-      expect(attempts.includes(Map({word: 'NORM', score: 4}))).toBe(true);
+      store = configureStore({attempts, matrix});
+      actions = bindActionCreators(actionCreators, store.dispatch);
+      helper.isOnDictionary = jest.fn().mockReturnValue(true);
     });
 
-    return promise;
-  });
-
-  it('invalidates word which is not on Board', () => {
-    const promise = actions.addCheckedAttempt('fake').then(() => {
-      const {attempts} = store.getState();
-      expect(attempts.includes(Map({word: 'FAKE', score: '✘'}))).toBe(true);
-    });
-
-    return promise;
-  });
-
-  it('invalidates word which is already on ScoreList', () => {
-    const promise = actions.addCheckedAttempt('hey').then(() => {
-      const {attempts} = store.getState();
-      expect(attempts.includes(Map({word: 'HEY', score: '✘'}))).toBe(true);
-    });
-
-    return promise;
-  });
-
-  it('invalidates word not on Dictionary', () => {
-    const promise = actions.addCheckedAttempt('hey')
-      .then(() => helper.isOnDictionary.mockReturnValue(false))
-      .then(() => actions.addCheckedAttempt('smv'))
-      .then(() => {
+    it('validates correct word', () => {
+      const promise = actions.addCheckedAttempt('norm').then(() => {
         const {attempts} = store.getState();
-        expect(attempts.includes(Map({word: 'SMV', score: '✘'}))).toBe(true);
+        expect(attempts.includes(Map({word: 'NORM', score: 4}))).toBe(true);
       });
 
-    return promise;
+      return promise;
+    });
+
+    it('invalidates word which is not on Board', () => {
+      const promise = actions.addCheckedAttempt('fake').then(() => {
+        const {attempts} = store.getState();
+        expect(attempts.includes(Map({word: 'FAKE', score: '✘'}))).toBe(true);
+      });
+
+      return promise;
+    });
+
+    it('invalidates word which is already on ScoreList', () => {
+      const promise = actions.addCheckedAttempt('hey').then(() => {
+        const {attempts} = store.getState();
+        expect(attempts.includes(Map({word: 'HEY', score: '✘'}))).toBe(true);
+      });
+
+      return promise;
+    });
+
+    it('invalidates word not on Dictionary', () => {
+      const promise = actions.addCheckedAttempt('hey')
+        .then(() => helper.isOnDictionary.mockReturnValue(false))
+        .then(() => actions.addCheckedAttempt('smv'))
+        .then(() => {
+          const {attempts} = store.getState();
+          expect(attempts.includes(Map({word: 'SMV', score: '✘'}))).toBe(true);
+        });
+
+      return promise;
+    });
   });
 });
