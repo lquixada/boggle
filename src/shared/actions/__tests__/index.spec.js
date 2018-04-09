b07490a81e0db0b6ce04917457d97775090c0855
Leonardo Quixadá <lquixada@gmail.com>
2018-04-08 22:29:51 -0300

    added more specs to the actions.

--- src/shared/actions/__tests__/index.spec.js
+++ src/shared/actions/__tests__/index.spec.js
@@ -4,6 +4,23 @@ import {bindActionCreators} from 'redux';
 import configureStore from '../../store';
 import * as actionCreators from '../../actions';
 import * as helper from '../../utils/helper';
+import {startGame, stopGame} from '../';
+
+describe('startGame', () => {
+  it('returns an action to start the game', () => {
+    expect(startGame()).toEqual({
+      type: 'START_GAME'
+    });
+  });
+});
+
+describe('stopGame', () => {
+  it('returns an action to stop the game', () => {
+    expect(stopGame()).toEqual({
+      type: 'STOP_GAME'
+    });
+  });
+});
 
 describe('addCheckedAttempt', () => {
   let actions;
