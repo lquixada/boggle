bb99524345e9e3ae749a7c90b2a2f42be58453c6
Leonardo Quixadá <lquixada@gmail.com>
2018-04-10 10:07:00 -0300

    removed production check for helmet and compression middleware.

--- src/server/index.js
+++ src/server/index.js
@@ -8,20 +8,19 @@ import staticMiddleware from './middlewares/static';
 import certbotController from './controllers/certbot';
 import indexController from './controllers/index';
 
+const {NODE_ENV} = process.env;
+
 // For semantic purposes, app is equivalent to server.
 const server = express();
 
 /* Middlewares */
-if (process.env.NODE_ENV === 'development') {
+if (NODE_ENV === 'development') {
   const webpackMiddlewares = require('./middlewares/webpack').default;
   server.use(...webpackMiddlewares);
 }
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(compression());
-  server.use(helmet());
-}
-
+server.use(helmet());
+server.use(compression(NODE_ENV === 'production'? -1: 0));
 server.use(loggerMiddleware);
 server.use(staticMiddleware);
 server.use(bodyParser.urlencoded({extended: true}));
