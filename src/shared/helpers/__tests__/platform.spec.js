51931d32729096af64b2bbdfd38726bbd765623c
Leonardo Quixadá <lquixada@gmail.com>
2018-05-03 20:31:37 -0300

    recovered coverage threshold.

---
+++ src/shared/helpers/__tests__/platform.spec.js
@@ -0,0 +1,31 @@
+
+import {isElectron} from '..';
+
+describe('Helpers', () => {
+  describe('isElectron', () => {
+    let _navigator;
+
+    beforeEach(() => {
+      _navigator = global.navigator;
+    });
+
+    afterEach(() => {
+      global.navigator = _navigator;
+    });
+
+    it('returns false when navigator is not present', () => {
+      delete global.navigator;
+      expect(isElectron()).toBe(false);
+    });
+
+    it('detects when it is Electron', () => {
+      global.navigator = {userAgent: ' Electron/2'};
+      expect(isElectron()).toBe(true);
+    });
+
+    it('detects when it is not Electron', () => {
+      global.navigator = {userAgent: ' Chrome/66'};
+      expect(isElectron()).toBe(false);
+    });
+  });
+});
