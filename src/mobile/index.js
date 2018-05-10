6b800bed9dcd2f73f8bebf550fe8548cbfca2c24
Leonardo Quixadá <lquixada@gmail.com>
2018-05-09 21:35:11 -0300

    added game play disclaimer for mobile version.

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -4,6 +4,13 @@ import {Game} from '../shared/components';
 import store from './store';
 
 export default class App extends Component {
+  componentDidMount() {
+    window.alert(`
+      Disclaimer: this is a prototype of a multi-platform app, not a product.
+      The gameplay is terrible, I know. Maybe it will improve some day.
+    `);
+  }
+
   render() {
     return (
       <Provider store={store}>
