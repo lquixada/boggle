ac8d0d2424c4d911d0182fc199f37d3d30faecc0
Leonardo Quixadá <lquixada@gmail.com>
2018-05-19 10:12:08 -0300

    updated Redux Dev Tools call.

--- src/client/store.js
+++ src/client/store.js
@@ -6,8 +6,8 @@ import configureStore from '../shared/store';
 import config from '../shared/config';
 
 const state = window.__INITIAL_STATE__;
-const devTools = (window.devToolsExtension ? window.devToolsExtension() : (f) => f);
 const middlewares = [routerMiddleware(history)];
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 if (process.env.NODE_ENV === 'production') {
   middlewares.push(ravenMiddleware(config.url.sentry));
