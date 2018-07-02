ea1ac6865e1e6b3c444abd76aa30830e0948797b
Leonardo Quixadá <lquixada@gmail.com>
2018-07-02 00:42:35 -0300

    refactored helpers url.

--- src/shared/helpers/url.js
+++ src/shared/helpers/url.js
@@ -1,10 +1,6 @@
 import {isElectron} from './platform'
 import config from '../config'
 
-export const publicPath = () => {
-  return isElectron() ? './public/images/' : `${config.cloudfront}/images/`
-}
+export const publicPath = () => isElectron() ? './public/images/' : `${config.cloudfront}/images/`
 
-export const img = (filename) => {
-  return `${publicPath()}${filename}`
-}
+export const img = (filename) => `${publicPath()}${filename}`
