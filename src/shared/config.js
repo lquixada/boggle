359219c8d548083a81d233de7c7b2954e9878b65
Leonardo Quixadá <lquixada@gmail.com>
2018-07-03 17:26:53 -0300

    fixed assets url.

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 export default {
-  cloudfront: isProd ? 'https://d2f4abhdzx92bs.cloudfront.net' : '',
+  cloudfront: isProd ? 'https://d2f4abhdzx92bs.cloudfront.net/' : '/',
   sentry: 'https://f42a178848284bf4ab21149d98b3768b@sentry.io/104332',
   wiktionary: 'https://en.wiktionary.org/w/api.php?action=query&format=json&origin=*'
 }
