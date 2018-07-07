620046b1c40d87051857e07743c2c3319a85229f
Leonardo Quixadá <lquixada@gmail.com>
2018-07-06 21:41:41 -0300

    fixed hot loader.

---
+++ src/client/hot-app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {hot} from 'react-hot-loader'
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import {renderRoutes} from 'react-router-config'
+
+import routes from '../shared/routes'
+import {App} from '../shared/components'
+import store from './store'
+
+const HotApp = () => (
+  <Provider store={store}>
+    <App>
+      <BrowserRouter>
+        {renderRoutes(routes)}
+      </BrowserRouter>
+    </App>
+  </Provider>
+)
+
+export default hot(module)(HotApp)
--- src/client/index.js
+++ src/client/index.js
@@ -1,34 +1,7 @@
 import 'es6-promise'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
-import {renderRoutes} from 'react-router-config'
-import {AppContainer} from 'react-hot-loader'
 
-import routes from '../shared/routes' // eslint-disable-line no-unused-vars
-import {App} from '../shared/components'
-import store from './store'
+import HotApp from './hot-app'
 
-const renderApp = () => {
-  const routes = require('../shared/routes').default
-
-  ReactDOM.hydrate(
-    <AppContainer>
-      <Provider store={store}>
-        <App>
-          <BrowserRouter>
-            {renderRoutes(routes)}
-          </BrowserRouter>
-        </App>
-      </Provider>
-    </AppContainer>
-    , document.getElementById('app')
-  )
-}
-
-renderApp()
-
-if (module.hot) {
-  module.hot.accept('../shared/routes', renderApp)
-}
+ReactDOM.hydrate(<HotApp />, document.getElementById('app'))
