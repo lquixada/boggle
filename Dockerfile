6d99544e24a0102d0720766d18625de0f6e192ad
Leonardo Quixadá <lquixada@gmail.com>
2019-01-19 19:42:54 -0500

    readded docker to the project.

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:10
+
+ENV NODE_ENV production
+
+RUN apt-get update
+
+RUN apt-get install -y htop vim
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --production
+
+COPY logs ./logs
+
+COPY web ./web
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,7 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 export default {
-  staticUrl: isProd ? process.env.ASSETS_DOMAIN : '/assets/',
+  staticUrl: process.env.ASSETS_DOMAIN || '/assets/',
   sentryUrl: 'https://f42a178848284bf4ab21149d98b3768b@sentry.io/104332',
   wiktionaryUrl: 'https://en.wiktionary.org/w/api.php?action=query&format=json&origin=*'
 }
